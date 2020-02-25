1a376bf39a51181924fe5ac1f8983201694f40bf
Martin Frichitthavong <martfric360@ed.amdsb.ca>
2020-02-25 13:25:42 -0500

    Add project files.

---
+++ Martin's Juice Shop/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Martin_s_Juice_Shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TitleText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textOne = new System.Windows.Forms.TextBox();
+            this.textTwo = new System.Windows.Forms.TextBox();
+            this.textThree = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.outputLabelOne = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TitleText
+            // 
+            this.TitleText.AutoSize = true;
+            this.TitleText.BackColor = System.Drawing.Color.Aqua;
+            this.TitleText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleText.Location = new System.Drawing.Point(13, 13);
+            this.TitleText.MinimumSize = new System.Drawing.Size(635, 40);
+            this.TitleText.Name = "TitleText";
+            this.TitleText.Size = new System.Drawing.Size(635, 40);
+            this.TitleText.TabIndex = 0;
+            this.TitleText.Text = "The Juice Shop";
+            this.TitleText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Fuchsia;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Berry Bomb";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lemon Drop";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(5, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Rainbow";
+            // 
+            // textOne
+            // 
+            this.textOne.Location = new System.Drawing.Point(99, 73);
+            this.textOne.Name = "textOne";
+            this.textOne.Size = new System.Drawing.Size(100, 20);
+            this.textOne.TabIndex = 4;
+            // 
+            // textTwo
+            // 
+            this.textTwo.Location = new System.Drawing.Point(99, 99);
+            this.textTwo.Name = "textTwo";
+            this.textTwo.Size = new System.Drawing.Size(100, 20);
+            this.textTwo.TabIndex = 5;
+            // 
+            // textThree
+            // 
+            this.textThree.Location = new System.Drawing.Point(99, 132);
+            this.textThree.Name = "textThree";
+            this.textThree.Size = new System.Drawing.Size(100, 20);
+            this.textThree.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "8.50$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(220, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "9.50$";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(212, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "10.50$";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(79, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Crimson;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 18);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Teathering";
+            this.label7.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(99, 240);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(79, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Pay";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Coral;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label8.Location = new System.Drawing.Point(24, 173);
+            this.label8.MinimumSize = new System.Drawing.Size(250, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(250, 50);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            this.label8.Visible = false;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(32, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Print Reciept";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(573, 429);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Buy Again";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // outputLabelOne
+            // 
+            this.outputLabelOne.AutoSize = true;
+            this.outputLabelOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabelOne.Location = new System.Drawing.Point(29, 315);
+            this.outputLabelOne.Name = "outputLabelOne";
+            this.outputLabelOne.Size = new System.Drawing.Size(0, 17);
+            this.outputLabelOne.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(270, 70);
+            this.label9.MinimumSize = new System.Drawing.Size(350, 350);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(350, 350);
+            this.label9.TabIndex = 18;
+            this.label9.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(667, 464);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.outputLabelOne);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textThree);
+            this.Controls.Add(this.textTwo);
+            this.Controls.Add(this.textOne);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TitleText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "The_Juice_Shop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textOne;
+        private System.Windows.Forms.TextBox textTwo;
+        private System.Windows.Forms.TextBox textThree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label outputLabelOne;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Martin's Juice Shop/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace Martin_s_Juice_Shop
+{
+
+    public partial class Form1 : Form
+    {
+      
+        const double oneJuice = 8.50;
+        const double twoJuice = 9.50;
+        const double threeJuice = 10.50;
+        const double taxRate = 0.13;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double numberofOne = Convert.ToDouble(textOne.Text);
+                double numberofTwo = Convert.ToDouble(textTwo.Text);
+                double numberofThree = Convert.ToDouble(textThree.Text);
+
+                double taxCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree)) * taxRate;
+                double totalCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree)) + taxCalculate;
+                double subCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree));
+               
+                outputLabelOne.Text = "Tax:     " + taxCalculate.ToString("C") + "\n" + "\n" + "SubTotal:     " + subCalculate.ToString("C") + "\n" + "\n" + "Total:     " + totalCalculate.ToString("C");
+                
+                textBox4.Visible = true; //Teathering 
+                label7.Visible = true; //shows tax total and sub
+                button2.Visible = true; //Pay Button
+            }
+
+            
+            catch
+            {
+                label8.Visible = true;
+                label8.Text = "TYPE Numbers ON ALL OPTIONS";//message warns person to type values on all options
+                Refresh();
+                Thread.Sleep(1000);
+
+                label8.Visible = false; 
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double numberofOne = Convert.ToDouble(textOne.Text);
+                double numberofTwo = Convert.ToDouble(textTwo.Text);
+                double numberofThree = Convert.ToDouble(textThree.Text);
+                double taxCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree)) * taxRate;
+                double totalCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree)) + taxCalculate;
+
+                double pay = Convert.ToDouble(textBox4.Text);
+                double cashBack = pay - totalCalculate;
+
+
+
+
+                button3.Visible = true; //Print RECIEPT BUTTON
+                outputLabelOne.Visible = true;
+                outputLabelOne.Text = "Cash Back:        " + cashBack.ToString("C") + "\n" + "\n" + "Amount Owing:        " + totalCalculate.ToString("C"); 
+
+                if (cashBack < 0) {
+                    
+                    button3.Visible = false;
+                    label8.Visible = true;
+                    label8.Text = "TYPE CORRECTAMOUNT";
+                    Refresh();
+                    Thread.Sleep(1000);
+                    label8.Text = "";
+                    label8.Visible = false;
+
+                
+                
+                
+                
+                }
+            }
+
+            catch
+            {
+                label8.Visible = true;
+                label8.Text = "TYPE a Number (No spaces)!!!!";
+               
+                Refresh();
+                Thread.Sleep(1000);
+                label8.Visible = false;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer chingSound = new SoundPlayer(Properties.Resources.Cha_Ching__1_);
+                void okay = chingSound.Play(); 
+
+                double numberofOne = Convert.ToDouble(textOne.Text);
+                double numberofTwo = Convert.ToDouble(textTwo.Text);
+                double numberofThree = Convert.ToDouble(textThree.Text);
+
+                double taxCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree)) * taxRate;
+                double totalCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree)) + taxCalculate;
+                double subCalculate = ((oneJuice * numberofOne) + (twoJuice * numberofTwo) + (threeJuice * numberofThree));
+
+                double pay = Convert.ToDouble(textBox4.Text);
+                double cashBack = pay - totalCalculate;
+
+                double priceofOne = oneJuice * numberofOne;
+                double priceofTwo = twoJuice * numberofTwo;
+                double priceofThree = threeJuice * numberofThree;
+
+                label9.Visible = true; //THE RECIEPT WILL DISPLAY:
+                Refresh();
+                label9.Text = "\n"+"                                       The Juice Shop" + "\n" + "                                     123 Unkown Street";
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "                                 Your Reciept Is Printing";
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Berry Bombs:        " + numberofOne + "       " + priceofOne.ToString("C");
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Lemon Drops:        " + numberofTwo + "      " + priceofTwo.ToString("C");
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Rainbows:        " + numberofThree + "      " + priceofThree.ToString("C");
+
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Sub Total:      " + subCalculate.ToString("C");
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Tax Total:      " + taxCalculate.ToString("C");
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     All Total:      " + totalCalculate.ToString("C");
+
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Paid With:      " + pay.ToString("C");
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "     Cash Back:      " + cashBack.ToString("C");
+                Refresh();
+                Thread.Sleep(500);
+                label9.Text += "\n" + "\n" + "\n" + "               Thanks For Shopping At The Juice Shop" + "\n" + "                     Please Come Again";
+                button4.Visible = true; //NEWGAME BUTTON!
+            }
+            catch
+            {
+                label8.Visible = true;
+                label8.Text = "TYPE a Number (No spaces)!!!!";
+
+                Refresh();
+                Thread.Sleep(1000);
+                label8.Visible = false;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label9.Visible = false;
+            label9.Text = "";
+            button4.Visible = false;
+            button3.Visible = false;
+            button2.Visible = false;
+            label7.Visible = false;
+            textBox4.Visible = false;
+            outputLabelOne.Visible = false;
+            textOne.Text = String.Empty;
+            textTwo.Text = String.Empty;
+            textThree.Text = String.Empty;
+            textBox4.Text = String.Empty;
+            
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
